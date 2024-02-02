567da626e01c22b2e1a0e1ab66656c44cdf7efc7
Aviel Bitton <aviel.bitton@nielsen.com>
2024-02-02 16:57:36 +0200

    chore: lint and pj update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,17 @@
 module.exports = {
+  env: {
+    browser: true,
+  },
   parser: '@typescript-eslint/parser',
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
   ],
-  plugins: ['@typescript-eslint', 'react', 'jsx-a11y', 'import'],
+  plugins: ['@typescript-eslint', 'react', 'import'],
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: 'module',
@@ -23,6 +25,10 @@ module.exports = {
     },
   },
   rules: {
-    // Add your own custom rules here
+    'react/react-in-jsx-scope': 'off',
+    'no-undef': ['error', { typeof: true }],
+  },
+  globals: {
+    module: 'writable',
   },
 }
